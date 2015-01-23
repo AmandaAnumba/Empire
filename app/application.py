8b8bb0175a25a84f17d13a6b8173267b4a3fa3fc
AmandaBrand <amanda.anumba@gmail.com>
2015-01-22 21:33:47 -0500

    updates

--- app/application.py
+++ app/application.py
@@ -7,8 +7,8 @@ from flask import render_template, session, url_for, escape
 
 @application.route("/")
 def index():
-    if session.get('username'):
-        username = escape(session['username'])
-        return render_template('home/dash_user.html', username=username)
-    else:
-        return render_template('home/index.html')
+    # if session.get('username'):
+    #     username = escape(session['username'])
+    #     return render_template('home/dash_user.html', username=username)
+    # else:
+    return render_template('home/index.html')
